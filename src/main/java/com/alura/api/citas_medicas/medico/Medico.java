5d5b26093fca98fe95fdc808e2bfcc213a152952
JonathanAPD <japd182@gmail.com>
2024-06-27 17:52:40 -0500

    Codificando Entidad Medico, clase Direccion y creando Repositorio IMedicoRepository

--- src/main/java/com/alura/api/citas_medicas/medico/Medico.java
+++ src/main/java/com/alura/api/citas_medicas/medico/Medico.java
@@ -1,4 +1,29 @@
 package com.alura.api.citas_medicas.medico;
 
+import com.alura.api.citas_medicas.direccion.Direccion;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String documento;
+    @Enumerated(EnumType.STRING)
+    private String Especialidad;
+    @Embedded
+    private Direccion direccion;
+
 }
