94bd164e24da98feebb23ea5f4b4cca6b60ac88c
JonathanAPD <japd182@gmail.com>
2024-07-04 22:29:30 -0500

    Creacion de AutenticaciónController.java, DTO DatosAutenticacionUsuario.java, Usuario.java para que implemente la Interface UserDetails y configuración de SecurityConfigurations.java con el metodo para usar AuthenticationManager y BCryptPasswordEncoder

---
+++ src/main/java/com/alura/api/citas_medicas/domain/usuarios/DatosAutenticacionUsuario.java
@@ -0,0 +1,4 @@
+package com.alura.api.citas_medicas.domain.usuarios;
+
+public record DatosAutenticacionUsuario(String login, String clave) {
+}
--- src/main/java/com/alura/api/citas_medicas/domain/usuarios/Usuario.java
+++ src/main/java/com/alura/api/citas_medicas/domain/usuarios/Usuario.java
@@ -5,6 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "usuarios")
 @Entity(name = "Usuario")
@@ -12,7 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +26,43 @@ public class Usuario {
     private String login;
     private String clave;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        //Asignar rol de usuario a todos los usuarios que se logueen el aplicación
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return clave;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+//        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+//        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+//        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+//        return UserDetails.super.isEnabled();
+    }
 }
