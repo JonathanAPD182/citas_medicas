6bd235a4bc335570e329923bce4aaceb671d5975
JonathanAPD <japd182@gmail.com>
2024-07-05 22:04:32 -0500

    creando ruta para variable del sistema que contiene el JWT_SECRET

--- src/main/java/com/alura/api/citas_medicas/controllers/AutenticacionController.java
+++ src/main/java/com/alura/api/citas_medicas/controllers/AutenticacionController.java
@@ -1,6 +1,9 @@
 package com.alura.api.citas_medicas.controllers;
 
 import com.alura.api.citas_medicas.domain.usuarios.DatosAutenticacionUsuario;
+import com.alura.api.citas_medicas.domain.usuarios.Usuario;
+import com.alura.api.citas_medicas.infra.security.DatosJwtToken;
+import com.alura.api.citas_medicas.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +22,17 @@ public class AutenticacionController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){
-        Authentication token = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(),
+        Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(),
                 datosAutenticacionUsuario.clave());
-        authenticationManager.authenticate(token);
+        var usuarioAutenticado = authenticationManager.authenticate(authToken);
+        var JwtToken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
         //Solo para fines didacticos vamos a retornar un 200 OK
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new DatosJwtToken(JwtToken));
     }
 
 }
---
+++ src/main/java/com/alura/api/citas_medicas/infra/security/DatosJwtToken.java
@@ -0,0 +1,5 @@
+package com.alura.api.citas_medicas.infra.security;
+
+public record DatosJwtToken(String jwtToken) {
+
+}
